314cb90badf32f8cf6192c7845ec38d0c4e0089b
Abhiram88 <76442123+Abhiram88@users.noreply.github.com>
2021-12-16 16:43:29 +0530

    completed signup route. Errors when logging in.

--- ReCode/server/services/render.js
+++ ReCode/server/services/render.js
@@ -1,12 +1,70 @@
+var userDb = require('../model/model');
+var bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken");
+
 exports.homeRoute = (req, res)=>{
     res.send("Home Route")
 }
 
-exports.loginRoute = (req, res)=>{
-    res.send("Login Route")
+exports.loginRoute = async (req, res)=>{
+    
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = userDb.findOne({email})
+    const saved_pass = userDb.findOne({email}, 'password')
+    
+    console.log("hello")
+    console.log(saved_pass.password)
+
+    if(user && await bcrypt.compare(password, saved_pass)){
+        const token = jwt.sign(
+            { user_id: email },
+            process.env.TOKEN_KEY,
+            {
+              expiresIn: "2h",
+            }
+          );
+    
+          // save user token
+          user.token = token;
+
+          res.send(user)
+    }
 }
 
 
-exports.signupRoute = (req, res)=>{
-    res.send("Login Route")
+exports.signupRoute = async (req, res)=>{
+
+    const {name, email, password } = req.body;
+
+    if(!(email && password && name)){
+        res.status(400).send("All input is required");
+    }
+    encryptedPassword = await bcrypt.hash(password, 10);
+
+    const user = new userDb({
+        name: req.body.name,
+        email: req.body.email,
+        gender:  req.body.gender,
+        status:  req.body.status,
+        password: encryptedPassword
+    })
+
+    // Create token
+    console.log(process.env.TOKEN_KEY);
+    const token = jwt.sign(
+        {user_id: email}, 
+        process.env.TOKEN_KEY,
+        {
+            expiresIn: "2h",
+        }
+    );
+
+    user.token = token;
+    user.save(user).then(data => {
+        //res.status(201).json(data);
+        res.redirect('/api/users');
+    })
+    
 }
